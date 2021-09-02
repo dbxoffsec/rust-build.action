941a8d8265ba02adc083b28cffaa9f43b43d7e97
Tom Samstag <teck@dropbox.com>
2021-09-02 13:40:02 -0700

    move the output directory into the workspace

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ if [ -z "${CMD_PATH+x}" ]; then
   export CMD_PATH=""
 fi
 
-OUTPUT_DIR="/output"
+OUTPUT_DIR="$GITHUB_WORKSPACE/output"
 mkdir -p "$OUTPUT_DIR"
 
 if ! FILE_LIST=$(/build.sh "$OUTPUT_DIR"); then
